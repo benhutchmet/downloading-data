9a8d3ad024c8becaaae02f204c6d1683c86be757
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2023-11-07 12:42:33 +0000

    testing download function

--- DAMIP/testing_download_functions.py
+++ DAMIP/testing_download_functions.py
@@ -14,6 +14,7 @@ def query_data_esgf(connection: SearchConnection,
                     variable_id: str,
                     table_id: str = 'Amon',
                     project: str = 'CMIP6',
+                    member_id: str = None,
                     latest: bool = True)-> List[Dict]:
     """
     Query the ESGF database for a given dataset.
@@ -30,8 +31,10 @@ def query_data_esgf(connection: SearchConnection,
         Variable name. E.g. 'tas'.
     table_id : str, optional
         Table name. E.g. 'Amon'. The default is 'Amon'.
-    project : str, optional
+    project : str, optiSonal
         Project name. E.g. 'CMIP6'. The default is 'CMIP6'.
+    member_id : str, optional
+        Member name. E.g. 'r1i1p1f1'. The default is None.
     latest : bool, optional
         Whether to download the latest version of the dataset. The default is True.
         
@@ -40,3 +43,30 @@ def query_data_esgf(connection: SearchConnection,
     results : list
         List of dictionaries containing the dataset information.
     """
+
+    if member_id is None:
+        # Query the database
+        query = connection.new_context(
+            latest=latest,
+            project=project,
+            source_id=source_id,
+            experiment_id=experiment_id,
+            variable_id=variable_id,
+            table_id=table_id
+        )
+    else:
+        # Query the database
+        query = connection.new_context(
+            latest=latest,
+            project=project,
+            source_id=source_id,
+            experiment_id=experiment_id,
+            variable_id=variable_id,
+            table_id=table_id,
+            member_id=member_id
+        )
+
+    # Get the results
+    results = query.search()
+
+    return results
