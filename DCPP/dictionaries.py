0c01c743db63c27db9d53a27b83c0f5fe60b5422
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2024-01-11 11:25:24 +0000

    looking at models for precip

--- DCPP/dictionaries.py
+++ DCPP/dictionaries.py
@@ -14,6 +14,9 @@ models_uas_vas = [ "BCC-CSM2-MR", "MPI-ESM1-2-HR", "CanESM5", "CMCC-CM2-SR5", "H
 # RSDS not valid for MPI-ESM1-2-LR
 models_rsds = [ "BCC-CSM2-MR", "MPI-ESM1-2-HR", "CanESM5", "CMCC-CM2-SR5", "HadGEM3-GC31-MM", "EC-Earth3", "FGOALS-f3-L", "MIROC6", "IPSL-CM6A-LR", "CESM1-1-CAM5-CMIP5", "NorCPM1" ]
 
+# Models for precip (pr)
+models_pr = [ "BCC-CSM2-MR", "MPI-ESM1-2-HR", "CanESM5", "CMCC-CM2-SR5", "HadGEM3-GC31-MM", "EC-Earth3", "MPI-ESM1-2-LR", "FGOALS-f3-L", "MIROC6", "IPSL-CM6A-LR", "NorCPM1" ]
+
 # variable models list dictionary
 var_models = {
     'tas': models,
